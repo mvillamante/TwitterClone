1d7f41f4e5a2d98d9bc4412840a96d64544ec96f
Noela Mae <andosayella13@gmail.com>
2024-02-11 00:58:28 +0800

    updated layout of homepage

---
+++ TwitterCloneUi/home.js
@@ -0,0 +1,22 @@
+let navlist = document.querySelectorAll('.navlist');
+
+function activeLink() {
+    for (let j = 0; j < navlist.length; j++) {
+        navlist[j].classList.remove('active');
+    }
+    this.classList.add('active');
+}
+
+
+navlist.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+
+let navUser = document.querySelector('.nav-user');
+let userPopup = document.querySelector('.user-popup');
+let showTriangle = document.querySelector('.triangle');
+
+navUser.onclick = function(){
+    userPopup.classList.toggle('active')
+    showTriangle.classList.toggle('active')
+}
