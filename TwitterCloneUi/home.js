48de4eeba3a6a3a2ad1b415210ae7b828f5f6863
Noela Mae <andosayella13@gmail.com>
2024-02-11 12:32:15 +0800

    updated media and layout for homepage

--- TwitterCloneUi/home.js
+++ TwitterCloneUi/home.js
@@ -20,3 +20,18 @@ navUser.onclick = function(){
     userPopup.classList.toggle('active')
     showTriangle.classList.toggle('active')
 }
+
+
+let viewpost = document.querySelector('.btn-container.viewpost');
+
+viewpost.addEventListener('click', function() {
+    viewpost.classList.toggle('active');
+    if (viewpost.classList.contains('active')) {
+        viewpost.style.pointerEvents = 'none';
+    } else {
+        viewpost.style.pointerEvents = 'auto'; 
+    }
+});
+
+
+
