698255bd25d44e3859fb41cb097a1dc02fd565e9
Noela Mae <andosayella13@gmail.com>
2024-02-12 15:30:48 +0800

    fixed login bug

--- TwitterCloneUi/login.js
+++ TwitterCloneUi/login.js
@@ -220,34 +220,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-<<<<<<< HEAD
-start();
-
-
-// play on load for gallery view
-setTimeout(transition, 1000);
-
-$('.js-trigger-transition').on('click', function(e) {
-  e.preventDefault();
-  transition();
-});
-
-function transition() {
-  var tl = new TimelineMax();
-  
-  tl.to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '50%' }, ease: Power2.easeOut}, 'close')
-  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '50%' }, ease: Power2.easeOut}, 'close')
-  .to($('.loader'), 0.2, {opacity: 1})
-  .to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '0%' }, ease: Power2.easeOut}, '+=1.5', 'open')
-  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '0%' }, ease: Power2.easeOut}, '-=0.2', 'open')
-  .to($('.loader'), 0.2, {opacity: 0}, '-=0.2');
-}
-=======
-async function helloWorld() {
-    const res = await fetch("http://localhost:3000/api")
-    const text = await res.text()
-    console.log(text);
-}
-
-start();
->>>>>>> 2fe8f124157ec75efa44c83de74858a2ca0a11c1
